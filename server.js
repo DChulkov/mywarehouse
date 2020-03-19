5372c15788eb4a2061112e613a580c8de173ccc6
unknown <mail@c1c.ru>
2020-03-20 00:19:28 +0300

    first commit5

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-'use strict';
+var http = require('http');
 
-const http = require('http');
-const http = require('fs');
+http.createServer(function (request, response) {
 
-const server = http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    console.log(request.method, request.url);
+    response.end('Hello World\n');
 
-    if (request.url == 'style/css') {
-        
-    }
+}).listen(8124);
 
-}
-    
-)   
+console.log('Server running at http://127.0.0.1:8124/');
